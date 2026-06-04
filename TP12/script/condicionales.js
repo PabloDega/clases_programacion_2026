ea6aa805ada36806be70642ad89743a15c25befd
Pablo <pablo@cfl60.com.ar>
2026-06-04 15:58:01 -0300

    TP14

--- TP12/script/condicionales.js
+++ TP12/script/condicionales.js
@@ -20,7 +20,7 @@ if(color == "verde"){
 
 // Cargar monto de compra ("window.prompt()") / Si > $50.000 descuento del 25% / Si > $20.000 descuento 10% - sino ningun descuento - devolver valor por consola ("console.log()")
 
-let monto = window.prompt("Ingresar monto de la compra");
+/* let monto = window.prompt("Ingresar monto de la compra");
 
 if(monto > 50000){
     console.log("25% de descuento");
@@ -31,4 +31,16 @@ if(monto > 50000){
 } else {
     console.log("Sin descuento");
     console.log("Valor a pagar:", (monto));
-} 
+}  */
+
+
+// Ejercicio - llevando mas de 10 pord por un total superior a $200.000 aplicar un descuento del 20% al total
+
+let total = window.prompt("Ingresar total");
+let cantidad = window.prompt("Ingresar cantidad");
+
+if(cantidad > 10 && total > 200000){
+    console.log(`Aplica descuento, cobrar: ${total * 0.8}`);
+} else {
+    console.log(`No aplica descuento, cobrar: ${total}`);
+}
---
+++ TP13/script/comillasYComentarios.js
@@ -0,0 +1,23 @@
+console.log("Ping con comillasYComentarios.js");
+
+// Comentario de linea
+// Se activa linea por linea
+// Se genera con "ctrl + }" o con "ctrl + ç cedilla"
+
+/* Comentario multilinea
+para bloques de codigo
+se genera con "alt + shift + a" */
+
+// Comillas para literales
+
+console.log("Comillado doble permite 'comilla simple'");
+console.log('Comillado simples permite "comilla doble"');
+console.log(`Backticks comillas 'simples' y "dobles"`);
+
+// Insertar variables
+
+const ejemplo = "texto de ejemplo";
+
+console.log("Agregando " + ejemplo + " con comillas dobles");
+console.log('Agregando ' + ejemplo + ' con comillas simples');
+console.log(`Agregando ${ejemplo} con backticks`);
