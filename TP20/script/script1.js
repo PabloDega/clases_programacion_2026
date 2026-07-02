50bf7b7d5225123445203f5e27d6b59577795ca5
Pablo <pablo@cfl60.com.ar>
2026-07-02 16:08:54 -0300

    TP20

--- TP20/script/script1.js
+++ TP20/script/script1.js
@@ -29,3 +29,45 @@ document
         .style.backgroundColor = e.target.dataset.color;
     })
 })
+
+// ================================
+//     Funcion tamaño y color
+// ================================
+let fontsize = 1;
+
+document.querySelector("#btnPequenio")
+.addEventListener("click", () => {
+    fontsize -= 0.05;
+    document.getElementById("texto")
+    .style.fontSize = fontsize + "em";
+});
+
+document.querySelector("#btnGrande")
+.addEventListener("click", () => {
+    fontsize += 0.05
+    document.getElementById("texto")
+    .style.fontSize = fontsize + "em";
+});
+
+document.querySelector("#selectColor")
+.addEventListener("change", (e) => {
+    //console.log(e.target.value);
+    document.getElementById("texto")
+    .style.color = e.target.value;
+});
+
+// ================================
+//        Ocultar y mostrar
+// ================================
+
+document.querySelector("#btnOcultar")
+.addEventListener("click", () => {
+    document.getElementById("toggle")
+    .style.opacity = 0;
+});
+
+document.querySelector("#btnMostrar")
+.addEventListener("click", () => {
+    document.getElementById("toggle")
+    .style.opacity = 1;
+});
